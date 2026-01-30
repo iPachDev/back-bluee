ecd1bd6fba4d69655f6b30087c679453e6a1cd71
Fernando Pacheco <fpacheco@bancobase.com>
2026-01-29 18:35:40 -0600

    primer tuning

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import * as dotenv from 'dotenv';
 import { UsersModule } from './users.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(UsersModule);
-  await app.listen(process.env.port ?? 3000);
+  dotenv.config();
+  const rabbitUrl = process.env.RABBITMQ_CONNECTION;
+  if (!rabbitUrl) {
+    throw new Error('RABBITMQ_CONNECTION is not set');
+  }
+  const queueName = process.env.MS_NAME_USERS;
+  if (!queueName) {
+    throw new Error('no se a definido el nombre del microservicio users');
+  }
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    UsersModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [rabbitUrl],
+        queue: queueName,
+        queueOptions: {
+          durable: true,
+        },
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
